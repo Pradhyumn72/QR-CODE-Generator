58f36db82dfa955f7849eae75b6c7805dde62fc1
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-05-17 18:03:11 +0530

    final completiton

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let imgbox=document.querySelector(".imgbox");
-let qrimg=document.querySelector(".qrimg");
-let qrtext=document.querySelector(".qrtext")
-let error=document.que
-let Generateqr=()=>{
-    if(qrtext.value.length >0){
-        qrimg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrtext.value;
-        imgbox.classList.add("show-img")
-    }
-    else{
-       
-    }
+let imgbox = document.querySelector(".imgbox");
+let qrimg = document.querySelector(".qrimg");
+let qrtext = document.querySelector(".qrtext");
 
-} 
+let Generateqr = () => {
+  if (qrtext.value.length > 0) {
+    qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrtext.value);
+    imgbox.classList.add("show-img");
+  } else {
+    qrtext.classList.add('error');
+    setTimeout(() => {
+      qrtext.classList.remove('error');
+    }, 1000);
+  }
+};
